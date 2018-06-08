083b437e18152d66bf87b368012fe06ce856a695
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-06-08 09:18:29 -0400

    .

---
+++ hackman/src/hackman/Move.java
@@ -0,0 +1,11 @@
+package hackman;
+
+public class Move {
+	
+	//return the list of nodes leading from our current position to the point x
+	public ArrayList<Node> moveTowards(Point x)
+	{
+		Node goal = new Node (x);
+	}
+
+}
